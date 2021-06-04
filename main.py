a6c523ee76541b352c734ae46c5b8b43eadd87d8
Luigi <>
2021-06-04 09:22:01 +0000

    insertion de ma page et de sa route

--- main.py
+++ main.py
@@ -28,4 +28,8 @@ def generate_user(username):
 def random_page():
   return render_template('page.html', code=choice(number_list))
 
+@web_site.route('/pageluigi')
+def page_de_luigi():
+  return render_template('page_luigi.html')
+
 web_site.run(host='0.0.0.0', port=8080)
