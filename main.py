3731b0bc1ec5190b3d02048c0b630adcacb2eda3
Marine JOLY <>
2021-06-04 09:18:22 +0000

    J'ai créeé la route pour accèder à ma page.

--- main.py
+++ main.py
@@ -28,4 +28,8 @@ def generate_user(username):
 def random_page():
   return render_template('page.html', code=choice(number_list))
 
+@web_site.route('/marine')
+def marine_page():
+    return render_template('page_marine.html')
+
 web_site.run(host='0.0.0.0', port=8080)
